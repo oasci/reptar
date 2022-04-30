ae6d86946131d498a7e307b0c3744ebce5b95b95
Alex Maldonado <aalexmmaldonado@gmail.com>
2022-04-30 17:03:17 -0400

    ci: attempt to fix pypi deploy

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@
 from setuptools import setup, find_packages
 import versioneer
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
 requirements = [
     'numpy', 'exdir>=0.4.2', 'cclib>=1.7.0', 'scipy'
 ]
@@ -17,25 +14,10 @@ setup_requirements = ['versioneer']
 test_requirements = requirements.append(['pytest', 'pytest-order'])
 
 setup(
-    author="Alex M. Maldonado",
-    author_email='aalexmmaldonado@gmail.com',
-    python_requires='>=3.7',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3'
-    ],
-    description="Implements reproducible data procedures",
     install_requires=requirements,
     extras_require={},
-    license="MIT license",
-    long_description=readme,
     include_package_data=True,
     package_data={'': ['definitions/*.yaml']},
-    keywords='reptar',
-    name='reptar',
     packages=find_packages(include=['reptar']),
     setup_requires=setup_requirements,
     test_suite='tests',
