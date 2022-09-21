2e6aa87b9776907c545c331b4b6ea993842de4d5
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2022-09-21 15:48:11 -0400

    ci: default redirect to main documentation

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -5,3 +5,8 @@ cd "${0%/*}"
 rm -rf ./html/
 sphinx-multiversion -nT ./source/ ./html/
 touch ./html/.nojekyll
+
+# Create html redirect to main
+echo "<head>" > ./html/index.html
+echo "  <meta http-equiv='refresh' content='0; URL=https://www.aalexmmaldonado.com/reptar/main/index.html'>" >> ./html/index.html
+echo "</head>" >> ./html/index.html
