067b66714ef9a955704c78e91644d7b4f05d4174
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2022-10-18 13:55:32 -0400

    feat: energy calculator driver

--- reptar/calculators/drivers.py
+++ reptar/calculators/drivers.py
@@ -28,6 +28,135 @@ try:
 except ImportError:
     pass
 
+class driverENERGY:
+    """Supervisor of energy+gradient workers.
+
+    Creates and manages ray tasks using specified worker.
+    """
+    def __init__(
+        self, Z, R, E, worker, worker_kwargs, n_cpus, n_cpus_worker=1,
+        chunk_size=50, start_slice=None, end_slice=None, ray_address='auto'
+    ):
+        """
+        Parameters
+        ----------
+        Z : :obj:`numpy.ndarray`, ndim: ``1``
+            Atomic numbers of the atoms with respect to ``R``. Should have shape
+            ``(i,)`` where ``i`` is the number of atoms in the system.
+        R : :obj:`numpy.ndarray`, ndim: ``3``
+            Cartesian coordinates of all structures in group. Should have shape
+            ``(j, i, 3)`` where ``j`` is the number of structures. Units are in
+            Angstroms.
+        E : :obj:`numpy.ndarray`, ndim: ``1``
+            Total electronic energies of all structures in ``R``. Energies that need
+            to be calculated should have ``NaN`` in the element corresponding
+            to the same index in ``R``. Should have shape ``(j,)``. Units are in
+            Hartrees.
+        worker : ``callable``
+            The desired worker function to compute energy and gradients. It should
+            be the same as any previous calculations. The ``ray.remote`` decorator
+            will be applied automatically.
+        worker_kwargs : :obj:`tuple`, ndim: ``1``
+            The other keyword arguments for the worker function after ``Z``,
+            ``R``, and ``R_idxs``.
+        n_cpus : :obj:`int`
+            Total number of CPUs we can use for ray tasks.
+        n_cpus_worker : :obj:`int`, default: ``1``
+            Number of CPUs to dedicate to each task.
+        chunk_size : :obj:`int`, default: ``50``
+            Number of calculations per task to do. This should be enough to make
+            the ray task overhead significantly less than calculations.
+        start_slice : :obj:`int`, default: ``None``
+            Trims ``R`` to start at this index.
+        end_slice : :obj:`int`, default: ``None``
+            Trims ``R`` to end at this index.
+        ray_address : :obj:`str`, default: ``'auto'``
+            Ray cluster address to connect to.
+        """
+        # Check arrays (obsessively)
+        assert R.ndim == 3
+        assert Z.shape[0] == R.shape[1]
+        assert R.shape[0] == E.shape[0]
+        assert R.shape[2] == 3
+
+        if not ray.is_initialized():
+            ray.init(address=ray_address)
+
+        # Storing arrays and other information
+        self.Z = ray.put(Z)
+        self.R = ray.put(R)
+        self.E = E
+        self.worker = worker
+        self.worker_kwargs = worker_kwargs
+        self.start_slice = start_slice
+        self.end_slice = end_slice
+
+        self.n_cpus = n_cpus
+        self.n_cpus_worker = n_cpus_worker
+        self.chunk_size = chunk_size
+        self.n_workers = math.floor(n_cpus/n_cpus_worker)
+    
+    def _idx_todo(self):
+        """Indices of missing energies (calculations to do).
+
+        Determines this by finding all ``NaN`` elements.
+        
+        Returns
+        -------
+        :obj:`numpy.ndarray`
+            Indices for ``R`` that are missing energie values.
+        """
+        E = self.E[self.start_slice:self.end_slice]
+        idx_todo = np.argwhere(np.isnan(E))[:,0]
+        return idx_todo
+    
+    def run(self, saver=None):
+        """Run the calculations.
+
+        Parameters
+        ----------
+        saver : :obj:`reptar.calculators.save.Saver`, optional
+            Save data after every worker finishes.
+
+        Returns
+        -------
+        :obj:`numpy.ndarray`
+            The total electronic energy array, ``E``, after all computations.
+        """
+        worker = ray.remote(self.worker)
+        idxs_todo = self._idx_todo()
+        chunker = chunk_iterable(idxs_todo, self.chunk_size)
+
+        # Initialize ray workers
+        workers = []
+        def add_worker(workers, chunker):
+            try:
+                chunk = list(next(chunker))
+                workers.append(
+                    worker.options(num_cpus=self.n_cpus_worker).remote(
+                        chunk, self.Z, self.R, **self.worker_kwargs
+                    )
+                )
+            except StopIteration:
+                pass
+        for _ in range(self.n_workers):
+            add_worker(workers, chunker)
+        
+        # Start calculations
+        while len(workers) != 0:
+            done_id, workers = ray.wait(workers)
+            
+            idx_done, E_done = ray.get(done_id)[0]
+            self.E[idx_done] = E_done
+
+            if saver is not None:
+                saver.save((self.E))
+            
+            add_worker(workers, chunker)
+        
+        return self.E
+
+
 class driverENGRAD:
     """Supervisor of energy+gradient workers.
 
